c8ef6f7bd80d8f227ce1cc936b68d8f75645154f
Andrew <hola.c@gmail.com>
2025-05-08 16:47:48 -0500

    Primer commit

---
+++ src/test/java/com/poli/programacion/ProgramacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poli.programacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
